848aa0d33eea956a34e746e51b2b24cc48555d7c
angie <amaticorena103@hotmail.com>
2022-03-07 20:57:15 -0500

    Adding buttons to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
-function NavBar() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+
   return (
-    <div>NavBar</div>
-  )
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Container>
+            <Toolbar>
+                <Typography variant="h6" className={classes.title}>
+                    Not sure
+                </Typography>
+                <Button color="inherit">Margarita List</Button>
+                <Button color="inherit">Add a Margarita</Button>
+            </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
 }
-
-export default NavBar
