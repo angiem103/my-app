8d4307f13c435828aae6c857d6b8a78649b856d5
angie <amaticorena103@hotmail.com>
2022-03-12 18:38:58 -0500

    styling NewMargarita form

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,8 @@ function Home() {
   return (
     <div>
         <h1 className='header'>Welcome to Margarita Alley</h1>
+        <h3 className='quote'> “If life gives you limes, make margaritas.” — Jimmy Buffett"</h3>
+
     </div>
   )
 }
