892b7b92174941ea6feb0fcd5b1169750a23a16e
angie <amaticorena103@hotmail.com>
2022-03-12 20:58:37 -0500

    adding Deleted button and fetch request

--- src/components/MargaritaCard.js
+++ src/components/MargaritaCard.js
@@ -2,10 +2,18 @@ import React from 'react'
 import { useParams } from "react-router-dom"
 import './style.css'
 
-function MargaritaCard({margaritas}) {
+function MargaritaCard({margaritas, onDrinkDelete}) {
 
   const params = useParams();
   const margarita = margaritas[params.id - 1];
+
+  function handleDelete() {
+    fetch(`http://localhost:3000/margaritas/${margarita.id}`, {
+      method: "DELETE"
+    }) 
+    .then(r => r.json())
+    .then(marg => onDrinkDelete(marg))
+  };
 
   return (
     <div className='text'>
@@ -16,6 +24,7 @@ function MargaritaCard({margaritas}) {
       <p>{margarita?.ingredients}</p>
       Directions:
       <p>{margarita?.directions}</p>
+      <button onClick={handleDelete}>Delete Recipe</button>
     </div>
   );
 };
